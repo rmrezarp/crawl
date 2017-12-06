869538fa436c039570a66e1062115ce9bbf66172
rmrezarp <rm.rezarp@gmail.com>
2017-12-06 21:13:19 +0700

    Update get_instagram v 2.0
    Add some new function

--- source/social media/get_instagram.py
+++ source/social media/get_instagram.py
@@ -6,40 +6,205 @@ Created on Thu Jun 22 10:55:21 2017
 """
 
 import urllib
+import sys
 import html
 import datetime
-import facebook
 import json
+import os
 import pandas as pd
-import psycopg2
+#import psycopg2
 import locale
 import numpy as np
 import time
+import requests
+from requests.exceptions import ConnectionError
+import bs4
+sys.path.append(os.path.abspath("E:/RM/besoklibur/python/code/"))
+from manipulate_data import *
 
-from bs4 import BeautifulSoup
 locale.setlocale(locale.LC_TIME, "id")
 
 id_fetchpost = '17880160963012870'
 id_fetchcomment = '17852405266163336'
+id_fetchexplore = '17875800862117404'
+
+#options = {0 : zero,
+#           1 : sqr,
+#           4 : sqr,
+#           9 : sqr,
+#           2 : even,
+#           3 : prime,
+#           5 : prime,
+#           7 : prime,
+#}
+
 
 def visit_profile(userid):
     url = 'https://www.instagram.com/' + userid + '/?__a=1'
+    dftemp = pd.DataFrame()
+    column = ['userid','username','followedby','following','mediacount','biography','url']
+    dfrow = make_temp_df(column)
+    response = get_json(url,'user')
+    if check_json("values",response,""):
+        dfrow['userid']      = response['user']['id']
+        dfrow['username']    = response['user']['username']
+        dfrow['followedby']  = response['user']["followed_by"]['count']
+        dfrow['following']   = response['user']["follows"]['count']
+        dfrow['mediacount']  = response['user']['media']['count']
+        dfrow['biography']   = response['user']['biography']
+        dfrow['url']         = response['user']['external_url']
+    #dataframe
+    dftemp = append_list_df(dftemp, dfrow)
+    return dftemp
+
+def visit_explore(tags):
+    url = 'https://www.instagram.com/explore/tags/' + tags + '/?__a=1'
+    try:
+        r = requests.get("http://example.com", timeout=0.001)
+    except ConnectionError as e:    # This is the correct syntax
+        r = "No response"
+    
     response =  urllib.request.urlopen(url).read().decode('utf8') 
-    return json.loads(response)
+    return json.loads(response)    
 
 
+def count_profile(df):
+    count = 0
+    users = []
 
-def get_post(json_prof,id_fetchpost):    
+    dffinal = pd.DataFrame()
+    for row in df.itertuples():
+        users.append(row.username)
+#        print(row.description)
+#        print(type(row.description))
+        count +=1
+#        print(len(words))
+#        print(words)
+#        if len(words) > 0:
+#            dffinal = append_list_df(dffinal,words)
+#        else:
+#            continue
+    dffinal = append_list_df(dffinal,users)
+    dffinal = dffinal.rename(columns={0: 'users'})
+    dffinal = count_tags(dffinal,"users")
+    return dffinal
+
+def get_profile(df):
+    dffinal1 = pd.DataFrame()
+    dffinal2 = pd.DataFrame()
+    dffinal = pd.DataFrame()
+    count = 1
+    for row in df.itertuples():
+        print("No. of profile : {}".format(count))
+        dftemp1 = visit_profile(row.users)
+        dftemp2 = fetch_phones(dftemp1.biography[0])
+        dffinal1 = append_list_df(dffinal1,dftemp1)
+        dffinal2 = append_list_df(dffinal2,dftemp2)
+        count+=1
+
+    dffinal = pd.concat([dffinal1, dffinal2], axis=1)
+    return dffinal
+
+
+def get_explore_post(dfpause,tag,id_fetch,hastoken,countpost):
+    hasnext = True
+    dffinal = pd.DataFrame()  
+    i = 1
+    while hasnext:
+        print("No of Iteration : {} and has next = {} ".format(i,hasnext) )
+        if hastoken:
+            url = 'https://www.instagram.com/graphql/query/?query_id=' + id_fetch + '&tag_name=' + tag + '&first=' + str(countpost) + '&after=' + token
+        else:
+            url = 'https://www.instagram.com/graphql/query/?query_id=' + id_fetch + '&tag_name=' + tag + '&first=' + str(countpost)
+        
+        response = get_json(url,"data")    
+ 
+        print('done get json')
+        print(url)
+
+        dffinal, token, hastoken = iterate_explore_post(response, dffinal,dfpause,tag)
+        dffinal['keyword'] = tag
+        hasnext = hastoken
+        i+=1
+        print(token)
+        if token == "DONE":
+            return dffinal
+        else:
+            continue
+    return dffinal
+
+def get_profile_post(json_prof,id_fetchpost,type_fetch):    
+        
     count_posts = json_prof['user']['media']['count']
     id_user = json_prof['user']['id']
-
     url = 'https://www.instagram.com/graphql/query/?query_id=' + id_fetchpost + '&id=' + id_user + '&first=' + str(count_posts)
-    response =  urllib.request.urlopen(url).read().decode('utf8') 
-    json_posts = json.loads(response)
-    return json_posts
+    response = get_json(url)
+    return response
+
+def get_data_from_post(link):
+    url = "https://www.instagram.com/p/" + link + "/?__a=1"
+    column = ['postid','username','userfullname','userid','description','countlikes','countcomments','posttimestamp','postlink']
+    dfrow = make_temp_df(column)
+    load = get_json(url,"graphql")
+    row = load['graphql']['shortcode_media']
+#    return row
+#    print(row['shortcode'])
+    dfrow['username']        = row['owner']['username']
+    dfrow['userfullname']       = row['owner']['full_name']
+    dfrow['userid']       = row['owner']['id']
+    dfrow['description']      = row['edge_media_to_caption']['edges'][0]['node']['text'] if check_json("values",row['edge_media_to_caption']['edges'],"") else np.NaN
+    
+    dfrow['countlikes']      = row['edge_media_preview_like']['count']
+    dfrow['countcomments']   = row['edge_media_to_comment']['count']
+    dfrow['posttimestamp']   = time.strftime("%D %H:%M:%S", time.localtime(int(row['taken_at_timestamp'])))
+    dfrow['postlink']        = row['shortcode']
+    dfrow['postid']          = row['id']
+    return dfrow
+
+#n = get_data_from_post("Bb_-ZnaHDF_")
+#n1 = get_data_from_post("BcNAnIFHhmE")
+
+#o = n['edge_media_to_caption']['edges'][0]
 
 
+#check_json("values",n['edge_media_to_caption']['edges'],"node")
 
+def iterate_explore_post(jsonposts, dffinal,dfpause,keyword):
+    dftemp = pd.DataFrame()
+    rows = jsonposts['data']['hashtag']['edge_hashtag_to_media']
+    flagdone = False
+    for row in rows['edges']:
+        postlink = row['node']['shortcode']
+        dfrow =  get_data_from_post(postlink)
+        
+#        postid = row['node']['id']
+#        postlink = row['node']['shortcode']
+#        ownerid = row['node']['owner']['id']
+#        ownername = get_username_from_post(postlink)
+#        posttimestamp = row['node']['taken_at_timestamp']
+#        posttimestamp = time.strftime("%D %H:%M:%S", time.localtime(int(posttimestamp)))
+#        countlikes = row['node']['edge_liked_by']['count']
+#        countcomments = row['node']['edge_media_to_comment']['count']
+#
+#        dfrow = []
+#        dfrow.append({'idowner' : str(ownerid), 'nameowner' : str(ownername),'idpost' : str(postid), 'linkpost': str(postlink), 'timestamp': str(posttimestamp), 'likescount' : str(countlikes), 'commentscount' : str(countcomments) })
+        if crawl_stop(dfrow,dfpause,"postid",keyword):
+            flagdone = True
+            break
+        else: 
+            dftemp = append_list_df(dftemp, dfrow)
+#            print("panjang dftemp = {}".format(len(dftemp)))
+            continue
+
+    dffinal = dffinal.append(dftemp)
+#    print("panjang df = {}".format(len(dffinal)))
+    if not flagdone:
+        nexttoken = rows['page_info']['end_cursor']
+    else:
+        nexttoken = "DONE"
+    hasnext = rows['page_info']['has_next_page']
+    return dffinal, nexttoken, hasnext
+  
 def iterate_post(json_posts,command):
     i = 1
     if command == 0:
@@ -117,13 +282,34 @@ def get_comments(id_owner, code_post,count_comments,id_fetchcomment):
     dframe = pd.DataFrame(df)
     return dframe
 
-prof = visit_profile('fwdlife_id')
-posts = get_post(prof,id_fetchpost)
-dffinal = iterate_post(posts,1)
-outfileurl = "E:\IDXP\FWD\instagram_post.csv"    
 
-dffinal.to_csv(outfileurl, sep='`', encoding='utf-8', index=False)
-df2 = pd.read_csv("E:\IDXP\FWD\instagram_post.csv", sep="`")
-    
-#    x = x.reset_index(drop=True)
-#    Y = get_comments('2138069033','BV9hEqwnpVA',5,id_fetchcomment)
+def instacrawl(dffinal,tag):
+    dfpause = get_first_row(dffinal)
+    dftemp = get_explore_post(dfpause,tag,id_fetchexplore,False,10)
+    dffinal = dffinal.append(dftemp)
+    dffinal = dffinal.sort_values(by='posttimestamp', ascending=False)
+    return dffinal
+
+#opentrip5[opentrip5['linkpost']=="BcCgwzTDuqO"]
+#
+#post = visit_explore('opentrip')
+#prof = visit_profile('fwdlife_id')
+#posts = get_post(prof,id_fetchpost)
+#dffinal = iterate_post(posts,1)
+#  
+#
+#
+#    
+##    x = x.reset_index(drop=True)
+##    Y = get_comments('2138069033','BV9hEqwnpVA',5,id_fetchcomment)
+#
+#
+#
+
+#opentrip = get_explore_post('opentrip',id_fetchexplore,False,10)
+#outfileurl = "E:/RM/besoklibur/database_03122017_new.csv"  
+#opentrip.to_csv(outfileurl, sep='`', encoding='utf-8', index=False)
+
+#opentrip6 = get_explore_post('opentrip',id_fetchexplore,False,10)
+#
+
