19de0ec48feb9a59b28bbb4d573ee3cc5ecc375e
rmrezarp <rm.rezarp@gmail.com>
2017-12-07 11:19:10 +0700

    testing commit
    Add description

--- source/social media/manipulate_data.py
+++ source/social media/manipulate_data.py
@@ -25,13 +25,13 @@ from requests.exceptions import ConnectionError
 
 def check_json(case,json,strings):
     if case == "key":
-        flag = True if strings in json else False        
-    if case == "values":        
+        flag = True if strings in json else False
+    if case == "values":
         flag = True if json != [] else False
     return flag
 
 def get_json(url,setname):
-    tries = 1  
+    tries = 1
     retries = 1
     jsontries = 1
     httptries = 1
@@ -44,19 +44,19 @@ def get_json(url,setname):
             print("timeout, tries : {}".format(retries))
             retries+=1
             continue
-        
+
         except requests.exceptions.RequestException as e:
             print("request exception, tries : {}".format(tries))
             tries+=1
             continue
 
-        
+
         except requests.exceptions.HTTPError as x:
             if x.status_code == 404:
                 print("HTTP Error ({}), tries : {}".format(x,httptries))
             httptries+=1
             continue
-        
+
         except ValueError:
             print("json empty, tries : {}".format(jsontries))
             jsontries+=1
@@ -65,7 +65,7 @@ def get_json(url,setname):
                 break
             else:
                 continue
-        
+
         try:
             if setname == "":
                 break
@@ -80,7 +80,7 @@ def get_json(url,setname):
         except ValueError:
             print("json empty, tries : {}".format(jsontries))
             jsontries+=1
-            continue            
+            continue
 
         break
     return response
@@ -104,11 +104,11 @@ def get_first_row(df):
     return df.iloc[0]
 
 def crawl_stop(dfcrawl,dfpause,field,keyword):
-    
+
 #    print("{} {}".format(dfcrawl[field], type(dfcrawl[field])))
 #    print("{} {}".format(dfpause[field], type(dfpause[field])))
-    
-    
+
+
     if str(dfcrawl[field]) <= str(dfpause[field]) and dfpause['keyword'] == keyword:
         print('sama')
         return True
@@ -157,44 +157,60 @@ def check_none(ob):
     else:
         return True
 
-def fetch_phones(strings):
+#get data from string value_counts
+#for now :
+#1. phone
+#2. email
+#3. website (url)
+#return data as pandas series
+def fetch_data(strings):
     column = ['phone','email','website']
     dfrow = make_temp_df(column)
     print(strings)
+
+#check if string is empty
     if not strings:
         dfrow['phone'] = []
         dfrow['email'] = []
-        dfrow['website'] = []        
-        return dfrow  
+        dfrow['website'] = []
+        return dfrow
     else:
 
+#using regex to get phone number, start with 8 (to get rid +62 or 0)
+#using digit between 10 to 25, in case of whitespace, example : 0856 55 30 38 28
         phoneObject = re.findall(r'(8[\d\s\-]{10,25})', strings)
-        emailObject = re.findall(r'([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)', strings)
+#cleaning trash like ' ' , '-', '\r', '\n'
         phoneObject = [words.replace(' ', '') for words in phoneObject]
         phoneObject = [words.replace('\r', '') for words in phoneObject]
         phoneObject = [words.replace('-', '') for words in phoneObject]
         phoneObject = ['0'+ words.replace('\n', '') for words in phoneObject]
-        
-    #    ((https?://|www.)+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*)
-    #    (www\.)[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)
-    #    websiteObject   = re.findall(r"(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/|www.)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?", strings)
-    #    websiteObject   = re.findall(r"(www\.)[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)", strings)
-    
+#using regex to get email
+#90% correct
+        emailObject = re.findall(r'([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)', strings)
+
+
+#using regex to get website
+#80% correct
+
         websiteObject   = re.findall(r"""(?i)\b((?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\b/?(?!@)))""", strings)
+#    ((https?://|www.)+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*)
+#    (www\.)[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)
+#    websiteObject   = re.findall(r"(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/|www.)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?", strings)
+#    websiteObject   = re.findall(r"(www\.)[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)", strings)
         dfrow['phone'] = phoneObject
         dfrow['email'] = emailObject
-        dfrow['website'] = websiteObject        
+        dfrow['website'] = websiteObject
         return dfrow
-    
-    
-def get_phones(df):
+
+
+def get_data(df):
     count = 0
     dffinal = pd.DataFrame()
     for row in df.itertuples():
         if pd.isnull(row.biography):
             continue
-        dftemp = fetch_phones(row.biography)
-#        print(row.description)
+        dftemp = fetch_data(row.biography)
+        #        print(row.description)
 #        print(type(row.description))
         count +=1
         dffinal = append_list_df(dffinal,dftemp)
