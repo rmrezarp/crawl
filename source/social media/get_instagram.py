18a54e3b66250e8e825c9586bcb4d6ba54df73ba
rmrezarp <rm.rezarp@gmail.com>
2017-12-07 16:18:24 +0700

    Description
    Added some description about function

--- source/social media/get_instagram.py
+++ source/social media/get_instagram.py
@@ -24,21 +24,25 @@ from manipulate_data import *
 
 locale.setlocale(locale.LC_TIME, "id")
 
+#variable to querying post
 id_fetchpost = '17880160963012870'
+#variable to querying comment
 id_fetchcomment = '17852405266163336'
+#variable to querying search post
 id_fetchexplore = '17875800862117404'
 
-#options = {0 : zero,
-#           1 : sqr,
-#           4 : sqr,
-#           9 : sqr,
-#           2 : even,
-#           3 : prime,
-#           5 : prime,
-#           7 : prime,
-#}
 
-
+#function to visiting get_profile
+#by giving its username
+#and get some data from it, for now list of data are :
+#1. userid
+#2. username
+#3. count of follower
+#4. count of following
+#5. count of post
+#6. biography
+#7. external url
+#returned value as dataframe row
 def visit_profile(userid):
     url = 'https://www.instagram.com/' + userid + '/?__a=1'
     dftemp = pd.DataFrame()
@@ -57,17 +61,20 @@ def visit_profile(userid):
     dftemp = append_list_df(dftemp, dfrow)
     return dftemp
 
-def visit_explore(tags):
-    url = 'https://www.instagram.com/explore/tags/' + tags + '/?__a=1'
-    try:
-        r = requests.get("http://example.com", timeout=0.001)
-    except ConnectionError as e:    # This is the correct syntax
-        r = "No response"
-    
-    response =  urllib.request.urlopen(url).read().decode('utf8') 
-    return json.loads(response)    
 
 
+#def visit_explore(tags):
+#    url = 'https://www.instagram.com/explore/tags/' + tags + '/?__a=1'
+#    try:
+#        r = requests.get("http://example.com", timeout=0.001)
+#    except ConnectionError as e:    # This is the correct syntax
+#        r = "No response"
+#
+#    response =  urllib.request.urlopen(url).read().decode('utf8')
+#    return json.loads(response)
+
+
+#function to get how many users and its posts
 def count_profile(df):
     count = 0
     users = []
@@ -75,20 +82,17 @@ def count_profile(df):
     dffinal = pd.DataFrame()
     for row in df.itertuples():
         users.append(row.username)
-#        print(row.description)
-#        print(type(row.description))
         count +=1
-#        print(len(words))
-#        print(words)
-#        if len(words) > 0:
-#            dffinal = append_list_df(dffinal,words)
-#        else:
-#            continue
     dffinal = append_list_df(dffinal,users)
     dffinal = dffinal.rename(columns={0: 'users'})
     dffinal = count_tags(dffinal,"users")
     return dffinal
 
+
+#function to get all data of a profile
+#from dataframe that contains profile nameowner
+#use 2 function. visit_profile and fetch_data
+#returned data as dataframe
 def get_profile(df):
     dffinal1 = pd.DataFrame()
     dffinal2 = pd.DataFrame()
@@ -97,7 +101,7 @@ def get_profile(df):
     for row in df.itertuples():
         print("No. of profile : {}".format(count))
         dftemp1 = visit_profile(row.users)
-        dftemp2 = fetch_phones(dftemp1.biography[0])
+        dftemp2 = fetch_data(dftemp1.biography[0])
         dffinal1 = append_list_df(dffinal1,dftemp1)
         dffinal2 = append_list_df(dffinal2,dftemp2)
         count+=1
@@ -106,9 +110,13 @@ def get_profile(df):
     return dffinal
 
 
+#function to get result from instagram search using a certain keyword / tag
+#searching stop when the data from frist row of database crawl equal or higher than crawled data
+#only from same keyword / tags
+#returned data as dataframe
 def get_explore_post(dfpause,tag,id_fetch,hastoken,countpost):
     hasnext = True
-    dffinal = pd.DataFrame()  
+    dffinal = pd.DataFrame()
     i = 1
     while hasnext:
         print("No of Iteration : {} and has next = {} ".format(i,hasnext) )
@@ -116,9 +124,9 @@ def get_explore_post(dfpause,tag,id_fetch,hastoken,countpost):
             url = 'https://www.instagram.com/graphql/query/?query_id=' + id_fetch + '&tag_name=' + tag + '&first=' + str(countpost) + '&after=' + token
         else:
             url = 'https://www.instagram.com/graphql/query/?query_id=' + id_fetch + '&tag_name=' + tag + '&first=' + str(countpost)
-        
-        response = get_json(url,"data")    
- 
+
+        response = get_json(url,"data")
+
         print('done get json')
         print(url)
 
@@ -133,65 +141,66 @@ def get_explore_post(dfpause,tag,id_fetch,hastoken,countpost):
             continue
     return dffinal
 
-def get_profile_post(json_prof,id_fetchpost,type_fetch):    
-        
+
+def get_profile_post(json_prof,id_fetchpost,type_fetch):
     count_posts = json_prof['user']['media']['count']
     id_user = json_prof['user']['id']
     url = 'https://www.instagram.com/graphql/query/?query_id=' + id_fetchpost + '&id=' + id_user + '&first=' + str(count_posts)
     response = get_json(url)
     return response
 
-def get_data_from_post(link):
+
+#function to get all data from a posts
+#by giving its postlink
+#and get some data from it, for now list of data are :
+#1. username
+#2. userfullname
+#3. userid
+#4. description
+#5. countlikes
+#6. countcomments
+#7. posttimestamp
+#8. postlink
+#9. postid
+#returned value as dataframe row
+def visit_posts(link):
     url = "https://www.instagram.com/p/" + link + "/?__a=1"
     column = ['postid','username','userfullname','userid','description','countlikes','countcomments','posttimestamp','postlink']
     dfrow = make_temp_df(column)
     load = get_json(url,"graphql")
     row = load['graphql']['shortcode_media']
-#    return row
-#    print(row['shortcode'])
-    dfrow['username']        = row['owner']['username']
-    dfrow['userfullname']       = row['owner']['full_name']
-    dfrow['userid']       = row['owner']['id']
+    dfrow['username']         = row['owner']['username']
+    dfrow['userfullname']     = row['owner']['full_name']
+    dfrow['userid']           = row['owner']['id']
     dfrow['description']      = row['edge_media_to_caption']['edges'][0]['node']['text'] if check_json("values",row['edge_media_to_caption']['edges'],"") else np.NaN
-    
-    dfrow['countlikes']      = row['edge_media_preview_like']['count']
-    dfrow['countcomments']   = row['edge_media_to_comment']['count']
-    dfrow['posttimestamp']   = time.strftime("%D %H:%M:%S", time.localtime(int(row['taken_at_timestamp'])))
-    dfrow['postlink']        = row['shortcode']
-    dfrow['postid']          = row['id']
+
+    dfrow['countlikes']       = row['edge_media_preview_like']['count']
+    dfrow['countcomments']    = row['edge_media_to_comment']['count']
+    dfrow['posttimestamp']    = time.strftime("%D %H:%M:%S", time.localtime(int(row['taken_at_timestamp'])))
+    dfrow['postlink']         = row['shortcode']
+    dfrow['postid']           = row['id']
     return dfrow
 
 #n = get_data_from_post("Bb_-ZnaHDF_")
 #n1 = get_data_from_post("BcNAnIFHhmE")
 
 #o = n['edge_media_to_caption']['edges'][0]
-
-
 #check_json("values",n['edge_media_to_caption']['edges'],"node")
 
+#function to iterate jsonpost and get all explore posts,
+#stop crawling explore post until id post on database
 def iterate_explore_post(jsonposts, dffinal,dfpause,keyword):
     dftemp = pd.DataFrame()
     rows = jsonposts['data']['hashtag']['edge_hashtag_to_media']
     flagdone = False
     for row in rows['edges']:
         postlink = row['node']['shortcode']
-        dfrow =  get_data_from_post(postlink)
-        
-#        postid = row['node']['id']
-#        postlink = row['node']['shortcode']
-#        ownerid = row['node']['owner']['id']
-#        ownername = get_username_from_post(postlink)
-#        posttimestamp = row['node']['taken_at_timestamp']
-#        posttimestamp = time.strftime("%D %H:%M:%S", time.localtime(int(posttimestamp)))
-#        countlikes = row['node']['edge_liked_by']['count']
-#        countcomments = row['node']['edge_media_to_comment']['count']
-#
-#        dfrow = []
-#        dfrow.append({'idowner' : str(ownerid), 'nameowner' : str(ownername),'idpost' : str(postid), 'linkpost': str(postlink), 'timestamp': str(posttimestamp), 'likescount' : str(countlikes), 'commentscount' : str(countcomments) })
+        dfrow =  visit_posts(postlink)
+
         if crawl_stop(dfrow,dfpause,"postid",keyword):
             flagdone = True
             break
-        else: 
+        else:
             dftemp = append_list_df(dftemp, dfrow)
 #            print("panjang dftemp = {}".format(len(dftemp)))
             continue
@@ -204,7 +213,8 @@ def iterate_explore_post(jsonposts, dffinal,dfpause,keyword):
         nexttoken = "DONE"
     hasnext = rows['page_info']['has_next_page']
     return dffinal, nexttoken, hasnext
-  
+
+
 def iterate_post(json_posts,command):
     i = 1
     if command == 0:
@@ -223,7 +233,7 @@ def iterate_post(json_posts,command):
             else:
                 desc_post = np.NaN
             print(desc_post)
-            
+
             df_comments = get_comments(id_owner, code_post, count_comments, id_fetchcomment)
             df = df.append(df_comments)
     #        if i == 1:
@@ -253,7 +263,7 @@ def iterate_post(json_posts,command):
                 desc_post = np.NaN
             print(desc_post)
             dflist.append({'idowner' : str(id_owner), 'count_comments' : count_comments, 'count_likes' : count_likes, 'idpost' : str(id_post), 'linkpost' : str(code_post), 'timestamp': str(timestamp_post), 'type_post' : str(type_post), 'desc_post' : str(desc_post)   })
-            
+
     #        if i == 1:
     #            df.append({'id_owner': id_owner,'id_post': id_post, 'link_post' : code_post, 'type_post': type_post, 'count_comments': count_comments, 'count_likes': count_likes, 'timestamp_post': timestamp_post, 'desc_post' : desc_post })
     #            break;
@@ -266,7 +276,7 @@ def iterate_post(json_posts,command):
 def get_comments(id_owner, code_post,count_comments,id_fetchcomment):
     print(count_comments)
     url = 'https://www.instagram.com/graphql/query/?query_id=' + id_fetchcomment + '&shortcode=' + code_post + '&first=' + str(count_comments)
-    response =  urllib.request.urlopen(url).read().decode('utf8') 
+    response =  urllib.request.urlopen(url).read().decode('utf8')
     df = []
     json_comments = json.loads(response)
     for row in json_comments['data']['shortcode_media']['edge_media_to_comment']['edges']:
@@ -274,7 +284,7 @@ def get_comments(id_owner, code_post,count_comments,id_fetchcomment):
         timestamp_text = row['node']['created_at']
         timestamp_text =  time.strftime("%D %H:%M:%S", time.localtime(int(timestamp_text)))
         idcomment_text = row['node']['id']
-        user_text = row['node']['owner']['username'] 
+        user_text = row['node']['owner']['username']
         iduser_text = row['node']['owner']['id']
         df.append({'idowner' : str(id_owner), 'idpost' : str(code_post), 'iduser': str(iduser_text), 'username': str(user_text), 'idcomment' : str(idcomment_text), 'timestamp': str(timestamp_text), 'comment': str(comment_text) })
 
@@ -282,34 +292,11 @@ def get_comments(id_owner, code_post,count_comments,id_fetchcomment):
     dframe = pd.DataFrame(df)
     return dframe
 
-
+#function to start crawling based on user_text
+#returned as dataframe
 def instacrawl(dffinal,tag):
     dfpause = get_first_row(dffinal)
     dftemp = get_explore_post(dfpause,tag,id_fetchexplore,False,10)
     dffinal = dffinal.append(dftemp)
     dffinal = dffinal.sort_values(by='posttimestamp', ascending=False)
     return dffinal
-
-#opentrip5[opentrip5['linkpost']=="BcCgwzTDuqO"]
-#
-#post = visit_explore('opentrip')
-#prof = visit_profile('fwdlife_id')
-#posts = get_post(prof,id_fetchpost)
-#dffinal = iterate_post(posts,1)
-#  
-#
-#
-#    
-##    x = x.reset_index(drop=True)
-##    Y = get_comments('2138069033','BV9hEqwnpVA',5,id_fetchcomment)
-#
-#
-#
-
-#opentrip = get_explore_post('opentrip',id_fetchexplore,False,10)
-#outfileurl = "E:/RM/besoklibur/database_03122017_new.csv"  
-#opentrip.to_csv(outfileurl, sep='`', encoding='utf-8', index=False)
-
-#opentrip6 = get_explore_post('opentrip',id_fetchexplore,False,10)
-#
-
--- source/social media/manipulate_data.py
+++ source/social media/manipulate_data.py
@@ -23,20 +23,30 @@ sys.path.append(os.path.abspath("E:/RM/besoklibur/python/code/"))
 from requests.exceptions import ConnectionError
 #from get_instagram_base import *
 
+locale.setlocale(locale.LC_TIME, "id")
+
+
+#function to checking json
+#if returned json are :
+#1. has the desired 'key' , strings = desired key
+#2. has values inside it, strings = ""
 def check_json(case,json,strings):
     if case == "key":
-        flag = True if strings in json else False        
-    if case == "values":        
+        flag = True if strings in json else False
+    if case == "values":
         flag = True if json != [] else False
     return flag
 
+#function to get json
+#by using exception when failing, so it keeps retrying
 def get_json(url,setname):
-    tries = 1  
+    tries = 1
     retries = 1
     jsontries = 1
     httptries = 1
 
     while True:
+#try requesting json
         try:
             response = requests.get(url).json()
 
@@ -44,19 +54,19 @@ def get_json(url,setname):
             print("timeout, tries : {}".format(retries))
             retries+=1
             continue
-        
+
         except requests.exceptions.RequestException as e:
             print("request exception, tries : {}".format(tries))
             tries+=1
             continue
 
-        
+
         except requests.exceptions.HTTPError as x:
             if x.status_code == 404:
                 print("HTTP Error ({}), tries : {}".format(x,httptries))
             httptries+=1
             continue
-        
+
         except ValueError:
             print("json empty, tries : {}".format(jsontries))
             jsontries+=1
@@ -65,12 +75,16 @@ def get_json(url,setname):
                 break
             else:
                 continue
-        
+
+#try checking json using check_json function
         try:
-            if setname == "":
+            if not check_json(response, setname):
                 break
-            if response[setname]:
-                break
+
+#            if setname == "":
+#                break
+#            if response[setname]:
+#                break
 
         except KeyError:
             print("json not loaded, tries : {}".format(jsontries))
@@ -80,35 +94,42 @@ def get_json(url,setname):
         except ValueError:
             print("json empty, tries : {}".format(jsontries))
             jsontries+=1
-            continue            
+            continue
 
         break
     return response
 
-
+#function to cleaning carriage
 def clean_carriage(df,colnames):
     df[colnames] = df[colnames].str.replace(r'\r\n', ' ')
     df[colnames] = df[colnames].str.replace(r'\r', ' ')
     df[colnames] = df[colnames].str.replace(r'\n', ' ')
     return df
 
+#function makes empty dataframe from lists
+#lists input as dataframce column __name__
 def make_temp_df(lists):
     dfrow = dict.fromkeys(lists)
     return dfrow
 
+#function to appending list into a dataframe dfrow
+#lists input as dataframce column __name__
 def append_list_df(df, lists):
     df = df.append(lists, ignore_index=True)
     return df
 
+#function to returning first row of dataframe
+#returned value as pandas series
 def get_first_row(df):
     return df.iloc[0]
 
+#function to stoping the crawling
 def crawl_stop(dfcrawl,dfpause,field,keyword):
-    
+
 #    print("{} {}".format(dfcrawl[field], type(dfcrawl[field])))
 #    print("{} {}".format(dfpause[field], type(dfpause[field])))
-    
-    
+
+#stop until desired column key and if only it has same keyword
     if str(dfcrawl[field]) <= str(dfpause[field]) and dfpause['keyword'] == keyword:
         print('sama')
         return True
@@ -117,6 +138,8 @@ def crawl_stop(dfcrawl,dfpause,field,keyword):
         return False
 
 
+#function to extract hashtags from a string
+#returned as list
 def fetch_hashtags(strings):
    hashtagsObject = re.findall(r'#([^#]+)', strings)
    hashtagsObject = ["#" + hashtags.lower() for hashtags in hashtagsObject]
@@ -124,10 +147,16 @@ def fetch_hashtags(strings):
 #   hashtagsObject = [words.replace(' ', '') for words in hashtagsObject]
    return hashtagsObject
 
+#function to makes summary table
+#based on 'colname' and get its count
 def count_tags(df,colname):
     df = df[colname].value_counts().reset_index().rename(columns={"index": colname, colname: "count"})
     return df
 
+
+#function to iterate dataframe and get its hashtags
+#from fetch_hashtags function
+#basically it's a iteration fetch_hashtags and added with some exception
 def get_hashtags(df):
     count = 0
     dffinal = pd.DataFrame()
@@ -151,51 +180,68 @@ def get_hashtags(df):
     dffinal = count_tags(dffinal,"words")
     return dffinal
 
+#function to check a data is empty or not
 def check_none(ob):
     if ob == None:
         return False
     else:
         return True
 
-def fetch_phones(strings):
+#get data from string value_counts
+#for now :
+#1. phone
+#2. email
+#3. website (url)
+#return data as pandas series
+def fetch_data(strings):
     column = ['phone','email','website']
     dfrow = make_temp_df(column)
     print(strings)
+
+#check if string is empty
     if not strings:
         dfrow['phone'] = []
         dfrow['email'] = []
-        dfrow['website'] = []        
-        return dfrow  
+        dfrow['website'] = []
+        return dfrow
     else:
 
+#using regex to get phone number, start with 8 (to get rid +62 or 0)
+#using digit between 10 to 25, in case of whitespace, example : 0856 55 30 38 28
         phoneObject = re.findall(r'(8[\d\s\-]{10,25})', strings)
-        emailObject = re.findall(r'([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)', strings)
+#cleaning trash like ' ' , '-', '\r', '\n'
         phoneObject = [words.replace(' ', '') for words in phoneObject]
         phoneObject = [words.replace('\r', '') for words in phoneObject]
         phoneObject = [words.replace('-', '') for words in phoneObject]
         phoneObject = ['0'+ words.replace('\n', '') for words in phoneObject]
-        
-    #    ((https?://|www.)+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*)
-    #    (www\.)[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)
-    #    websiteObject   = re.findall(r"(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/|www.)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?", strings)
-    #    websiteObject   = re.findall(r"(www\.)[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)", strings)
-    
-        websiteObject   = re.findall(r"""(?i)\b((?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\b/?(?!@)))""", strings)
+#using regex to get email
+#90% correct
+        emailObject = re.findall(r'([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)', strings)
+
+
+#using regex to get website
+#80% correct
+
+        websiteObject = re.findall(r"""(?i)\b((?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\b/?(?!@)))""", strings)
+#    ((https?://|www.)+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*)
+#    (www\.)[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)
+#    websiteObject   = re.findall(r"(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/|www.)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?", strings)
+#    websiteObject   = re.findall(r"(www\.)[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)", strings)
         dfrow['phone'] = phoneObject
         dfrow['email'] = emailObject
-        dfrow['website'] = websiteObject        
+        dfrow['website'] = websiteObject
         return dfrow
-    
-    
-def get_phones(df):
+
+#function to iterate dataframe and get its data
+#from fetch_data function
+#basically it's a iteration fetch_data and added with some exception
+def get_data(df):
     count = 0
     dffinal = pd.DataFrame()
     for row in df.itertuples():
         if pd.isnull(row.biography):
             continue
-        dftemp = fetch_phones(row.biography)
-#        print(row.description)
-#        print(type(row.description))
+        dftemp = fetch_data(row.biography)
         count +=1
         dffinal = append_list_df(dffinal,dftemp)
         if count == 30:
