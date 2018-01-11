7ef1c5e9fe9166341af13fdf8bb4baca5d4c7266
rmrezarp <rm.rezarp@gmail.com>
2018-01-11 13:26:45 +0700

    Revision
    No change

--- source/social media/manipulate_data.py
+++ source/social media/manipulate_data.py
@@ -286,3 +286,4 @@ def normalize_text(text):
     result = re.sub(r'[^a-z0-9 -]', ' ', result, flags = re.IGNORECASE|re.MULTILINE)
     result = re.sub(r'( +)', ' ', result, flags = re.IGNORECASE|re.MULTILINE)
     return result
+
